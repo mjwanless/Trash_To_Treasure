3e1943f75f4e73236f565dbf17762b8890c6f0df
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-10-31 21:43:04 -0700

    Allow firebase js to be viewable

---
+++ scripts/firebaseAPI_DTC13.js
@@ -0,0 +1,18 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
+    apiKey: "AIzaSyDUBCrUPB5D6mq_tHmlwAR0zKWai0kzOrw",
+    authDomain: "comp-1800-group-project.firebaseapp.com",
+    projectId: "comp-1800-group-project",
+    storageBucket: "comp-1800-group-project.appspot.com",
+    messagingSenderId: "400433726157",
+    appId: "1:400433726157:web:598065e5e8565fc80e9f84"
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
