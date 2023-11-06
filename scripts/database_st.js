be0b88cad8f601c9dc34a62f48f57e886db8febf
Sebastian Taylor <staylor173@my.bcit.ca>
2023-11-05 19:34:01 -0800

    metal database

--- scripts/database_st.js
+++ scripts/database_st.js
@@ -1,22 +1,23 @@
 function display_recycling_information(collection) {
     db.collection(collection)
-        .doc("glass")
+        .doc("metal")
         .get()
         .then(doc => {
-            glass_data = doc.data().glass;
-            console.log(glass_data["type"])
-            $("#type-textbox").html(glass_data["type"])
+            glass_data = doc.data().metal;
 
-            console.log(glass_data["description"])
-            $(".details-field").html(glass_data["description"])
+            console.log(metal_data["type"])
+            $("#type-textbox").html(metal_data["type"])
 
-            console.log(glass_data["name"])
-            $("#name-textbox").html(glass_data["name"])
+            console.log(metal_data["description"])
+            $(".details-field").html(metal_data["description"])
 
-            console.log(glass_data["recyclable"])
-            $("#recyclable-textbox").html(glass_data["recyclable"])
+            console.log(metal_data["name"])
+            $("#name-textbox").html(metal_data["name"])
 
-            console.log(glass_data)
+            console.log(metal_data["recyclable"])
+            $("#recyclable-textbox").html(metal_data["recyclable"])
+
+            console.log(metal_data)
 
         //     anthonyhelp = doc.data().test;
         //     console.log(anthonyhelp["nest1"][0])
@@ -25,10 +26,15 @@ function display_recycling_information(collection) {
 
 function display_depot_locations(collection) {
     db.collection(collection)
-        .doc("5oHNEmdKpdoAQmPM7ff1")
+        .doc("depot_locations")
         .get()
         .then(doc => {
-            depot_data = doc.data().placeholder;
+            depot1_data = doc.data().capital_salvage;
+            depot2_data = doc.data().go_green_bottle_depot_and_recycling;
+            depot3_data = doc.data().north_shore_bottle_depot_and_recycling;
+            $("#details-field-where-1").html(depot1_data["address"]);
+            $("#details-field-where-2").html(depot2_data["address"]);
+            $("#details-field-where-3").html(depot3_data["address"]);
             console.log(depot_data)
         })
 }
