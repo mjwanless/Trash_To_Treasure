d7d0cfd349fd853dcb0a059b932dc8b447bccd8c
Malcolm Wanless <mwanless2@my.bcit.ca>
2023-11-05 19:27:20 -0800

    database connected with the materials page

--- scripts/database_mjw.js
+++ scripts/database_mjw.js
@@ -4,22 +4,10 @@ function display_recycling_information(collection) {
         .get()
         .then((doc) => {
             plastic_data = doc.data().plastic;
-            console.log(plastic_data["type"]);
             $("#type-textbox").html(plastic_data["type"]);
-
-            console.log(plastic_data["description"]);
             $(".details-field").html(plastic_data["description"]);
-
-            console.log(plastic_data["name"]);
             $("#name-textbox").html(plastic_data["name"]);
-
-            console.log(plastic_data["recyclable"]);
             $("#recyclable-textbox").html(plastic_data["recyclable"]);
-
-            console.log(plastic_data);
-
-            //     anthonyhelp = doc.data().test;
-            //     console.log(anthonyhelp["nest1"][0])
         });
 }
 
@@ -31,10 +19,9 @@ function display_depot_locations(collection) {
             depot1_data = doc.data().capital_salvage;
             depot2_data = doc.data().go_green_bottle_depot_and_recycling;
             depot3_data = doc.data().north_shore_recycling_and_waste_centre;
-            $("");
-            console.log(depot1_data);
-            console.log(depot2_data);
-            console.log(depot3_data);
+            $("#details-field-where-1").html(depot1_data["address"]);
+            $("#details-field-where-2").html(depot2_data["address"]);
+            $("#details-field-where-3").html(depot3_data["address"]);
         });
 }
 
