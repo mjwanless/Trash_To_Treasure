047c0e93595eaf3485b1e7ff6d83e90b4262c3cc
Malcolm Wanless <mwanless2@my.bcit.ca>
2023-11-05 19:23:48 -0800

    structure changes for displaying location data

--- scripts/database_mjw.js
+++ scripts/database_mjw.js
@@ -1,37 +1,42 @@
 function display_recycling_information(collection) {
     db.collection(collection)
-        .doc("glass")
+        .doc("plastic")
         .get()
-        .then(doc => {
-            glass_data = doc.data().glass;
-            console.log(glass_data["type"])
-            $("#type-textbox").html(glass_data["type"])
+        .then((doc) => {
+            plastic_data = doc.data().plastic;
+            console.log(plastic_data["type"]);
+            $("#type-textbox").html(plastic_data["type"]);
 
-            console.log(glass_data["description"])
-            $(".details-field").html(glass_data["description"])
+            console.log(plastic_data["description"]);
+            $(".details-field").html(plastic_data["description"]);
 
-            console.log(glass_data["name"])
-            $("#name-textbox").html(glass_data["name"])
+            console.log(plastic_data["name"]);
+            $("#name-textbox").html(plastic_data["name"]);
 
-            console.log(glass_data["recyclable"])
-            $("#recyclable-textbox").html(glass_data["recyclable"])
+            console.log(plastic_data["recyclable"]);
+            $("#recyclable-textbox").html(plastic_data["recyclable"]);
 
-            console.log(glass_data)
+            console.log(plastic_data);
 
-        //     anthonyhelp = doc.data().test;
-        //     console.log(anthonyhelp["nest1"][0])
-        })
+            //     anthonyhelp = doc.data().test;
+            //     console.log(anthonyhelp["nest1"][0])
+        });
 }
 
 function display_depot_locations(collection) {
     db.collection(collection)
-        .doc("5oHNEmdKpdoAQmPM7ff1")
+        .doc("depot_locations")
         .get()
-        .then(doc => {
-            depot_data = doc.data().placeholder;
-            console.log(depot_data)
-        })
+        .then((doc) => {
+            depot1_data = doc.data().capital_salvage;
+            depot2_data = doc.data().go_green_bottle_depot_and_recycling;
+            depot3_data = doc.data().north_shore_recycling_and_waste_centre;
+            $("");
+            console.log(depot1_data);
+            console.log(depot2_data);
+            console.log(depot3_data);
+        });
 }
 
 display_recycling_information("materials");
-display_depot_locations("locations")
+display_depot_locations("locations");
