29d97645db7a37b5df9cc15cd8318b0fe0c5ca38
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-11-05 18:18:53 -0800

    Polish database_first_try js file

--- scripts/database_first_try.js
+++ scripts/database_first_try.js
@@ -1,4 +1,4 @@
-function display_recycling_information_from_database(collection) {
+function display_recycling_information(collection) {
     db.collection(collection)
         .doc("glass")
         .get()
@@ -22,37 +22,16 @@ function display_recycling_information_from_database(collection) {
         //     console.log(anthonyhelp["nest1"][0])
         })
 }
-// function display_recycling_information_from_database(collection) {
-//     db.collection(collection)
-//         .doc("5oHNEmdKpdoAQmPM7ff1")
-//         .get()
-//         .then(doc => {
-//             location1 = doc.data().Capital
-//         })
 
-// }
+function display_depot_locations(collection) {
+    db.collection(collection)
+        .doc("5oHNEmdKpdoAQmPM7ff1")
+        .get()
+        .then(doc => {
+            depot_data = doc.data().placeholder;
+            console.log(depot_data)
+        })
+}
 
-display_recycling_information_from_database("materials");
-// display_recycling_information_from_database("locations");
-
-// function displayHikeInfo() {
-//     let params = new URL(window.location.href); //get URL of search bar
-//     let ID = params.searchParams.get("docID"); //get value for key "id"
-//     console.log(ID);
-
-//     // doublecheck: is your collection called "Reviews" or "reviews"?
-//     db.collection("hikes")
-//         .doc(ID)
-//         .get()
-//         .then(doc => {
-//             thisHike = doc.data();
-//             hikeCode = thisHike.code;
-//             hikeName = doc.data().name;
-
-//             // only populate title, and image
-//             document.getElementById("hikeName").innerHTML = hikeName;
-//             let imgEvent = document.querySelector(".hike-img");
-//             imgEvent.src = "../images/" + hikeCode + ".jpg";
-//         });
-// }
-// displayHikeInfo();
+display_recycling_information("materials");
+display_depot_locations("locations")
