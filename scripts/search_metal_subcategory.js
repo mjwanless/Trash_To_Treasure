1ea6d014f47f700c26a8f4914d94b0b53ae5f695
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-11-11 21:53:50 -0800

    Add functionality to homepage and search buttons

---
+++ scripts/search_metal_subcategory.js
@@ -0,0 +1,15 @@
+function display_material_subcategories(collection, category) {
+    db.collection(collection)
+        .doc(`${category}`)
+        .get()
+        .then(doc => {
+            category_data = doc.data();
+            const fields = Object.keys(category_data)
+            console.log(fields)
+            console.log(fields[0])
+            for (i = 0; i <= fields.length; i++)
+                $(`.button${i}`).html(fields[i])
+        })
+}
+
+display_material_subcategories("materials", "metal")
