0ac22ab6c0cd05f191d70fec3f80656343b9274c
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-11-12 20:03:09 -0800

    FIrst draft of js code to show material info

---
+++ scripts/load_materials.js
@@ -0,0 +1,48 @@
+const user_selected_subcategory = localStorage.getItem("user_selected_subcategory")
+
+function dynamically_load_correct_script() {
+    console.log(user_selected_subcategory)
+    if (user_selected_subcategory == 'subcategory_1') {
+        var added_script = document.createElement('script')
+        added_script.src = ".js"
+        document.body.appendChild(added_script)
+    }
+    if (user_selected_subcategory == 'subcategory_2') {
+        var added_script = document.createElement('script')
+        added_script.src = ".js"
+        document.body.appendChild(added_script)
+    }
+    if (user_selected_subcategory == 'subcategory_3') {
+        var added_script = document.createElement('script')
+        added_script.src = ".js"
+        document.body.appendChild(added_script)
+    }
+    if (user_selected_subcategory == 'subcategory_4') {
+        var added_script = document.createElement('script')
+        added_script.src = ".js"
+        document.body.appendChild(added_script)
+    }
+    if (user_selected_subcategory == 'subcategory_5') {
+        var added_script = document.createElement('script')
+        added_script.src = ".js"
+        document.body.appendChild(added_script)
+    }
+    if (user_selected_subcategory == 'subcategory_6') {
+        var added_script = document.createElement('script')
+        added_script.src = ".js"
+        document.body.appendChild(added_script)
+    }
+    if (user_selected_subcategory == 'subcategory_7') {
+        var added_script = document.createElement('script')
+        added_script.src = ".js"
+        document.body.appendChild(added_script)
+    }
+    if (user_selected_subcategory == 'subcategory_8') {
+        var added_script = document.createElement('script')
+        added_script.src = ".js"
+        document.body.appendChild(added_script)
+    }
+    
+}
+
+dynamically_load_correct_script()
