f492ff2b839f58a3cb495fe4e9f331d4b983ea07
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-11-12 19:00:02 -0800

    Remove underscores when displaying names

--- scripts/search_glass_subcategory.js
+++ scripts/search_glass_subcategory.js
@@ -8,7 +8,7 @@ function display_material_subcategories(collection, category) {
             console.log(fields)
             console.log(fields[0])
             for (i = 0; i <= fields.length; i++)
-                $(`.button${i}`).html(fields[i])
+                $(`.button${i}`).html(fields[i].replace("_", " "))
         })
 }
 
