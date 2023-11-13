0b51833f0758c91e4f23a7cbd63d81e4ac8d2abd
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-11-12 23:03:20 -0800

    Change name of files to improve readability

---
+++ scripts/material_subcategories.js
@@ -0,0 +1,102 @@
+function redirect_user_to_display_material_information() {
+    window.location.href = 'display_material_information.html'
+}
+
+function store_subcategory_1_in_local_storage() {
+    var subcategory = document.getElementsByClassName("button0")[0].innerHTML;
+    localStorage.setItem('user_selected_subcategory', subcategory)
+}
+function store_subcategory_2_in_local_storage() {
+    var subcategory = document.getElementsByClassName("button1")[0].innerHTML;
+    localStorage.setItem('user_selected_subcategory', subcategory)
+}
+function store_subcategory_3_in_local_storage() {
+    var subcategory = document.getElementsByClassName("button2")[0].innerHTML;
+    localStorage.setItem('user_selected_subcategory', subcategory)
+}
+function store_subcategory_4_in_local_storage() {
+    var subcategory = document.getElementsByClassName("button3")[0].innerHTML;
+    localStorage.setItem('user_selected_subcategory', subcategory)
+}
+function store_subcategory_5_in_local_storage() {
+    var subcategory = document.getElementsByClassName("button4")[0].innerHTML;
+    localStorage.setItem('user_selected_subcategory', subcategory)
+}
+function store_subcategory_6_in_local_storage() {
+    var subcategory = document.getElementsByClassName("button5")[0].innerHTML;
+    localStorage.setItem('user_selected_subcategory', subcategory)
+}
+function store_subcategory_7_in_local_storage() {
+    var subcategory = document.getElementsByClassName("button6")[0].innerHTML;
+    localStorage.setItem('user_selected_subcategory', subcategory)
+}
+function store_subcategory_8_in_local_storage() {
+    var subcategory = document.getElementsByClassName("button7")[0].innerHTML;
+    localStorage.setItem('user_selected_subcategory', subcategory)
+}
+
+
+document.addEventListener("click", function (e) {
+    const category_button = e.target.closest(".button0");
+    if (category_button) {
+        console.log("working!");
+        redirect_user_to_display_material_information()
+        store_subcategory_1_in_local_storage()
+    }
+})
+document.addEventListener("click", function (e) {
+    const category_button = e.target.closest(".button1");
+    if (category_button) {
+        console.log("working!");
+        redirect_user_to_display_material_information()
+        store_subcategory_2_in_local_storage()
+    }
+})
+document.addEventListener("click", function (e) {
+    const category_button = e.target.closest(".button2");
+    if (category_button) {
+        console.log("working!");
+        redirect_user_to_display_material_information()
+        store_subcategory_3_in_local_storage()
+    }
+})
+document.addEventListener("click", function (e) {
+    const category_button = e.target.closest(".button3");
+    if (category_button) {
+        console.log("working!");
+        redirect_user_to_display_material_information()
+        store_subcategory_4_in_local_storage()
+    }
+})
+document.addEventListener("click", function (e) {
+    const category_button = e.target.closest(".button4");
+    if (category_button) {
+        console.log("working!");
+        redirect_user_to_display_material_information()
+        store_subcategory_5_in_local_storage()
+    }
+})
+document.addEventListener("click", function (e) {
+    const category_button = e.target.closest(".button5");
+    if (category_button) {
+        console.log("working!");
+        redirect_user_to_display_material_information()
+        store_subcategory_6_in_local_storage()
+    }
+})
+document.addEventListener("click", function (e) {
+    const category_button = e.target.closest(".button6");
+    if (category_button) {
+        console.log("working!");
+        redirect_user_to_display_material_information()
+        store_subcategory_7_in_local_storage()
+    }
+})
+document.addEventListener("click", function (e) {
+    const category_button = e.target.closest(".button7");
+    if (category_button) {
+        console.log("working!");
+        redirect_user_to_display_material_information()
+        store_subcategory_8_in_local_storage()
+    }
+})
