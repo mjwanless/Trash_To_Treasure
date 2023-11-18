b38407b85c9b56b5512aba554b0472d19ad63357
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-11-18 13:11:23 -0800

    Reduce js code length

--- scripts/load_subcategories.js
+++ scripts/load_subcategories.js
@@ -1,42 +1,30 @@
 // Retrieve the user-selected category from local storage.
 const user_selected_category = localStorage.getItem("user_selected_category")
 
-// Function to dynamically load a specific script based on the selected category.
-function dynamically_load_correct_script() {
-    console.log(user_selected_category)
+// Execute the function to load the appropriate script for the user-selected category.
+function display_material_subcategories(collection, user_selected_category) {
+    // Access the specified database collection.
+    db.collection(collection)
+        .doc(`${user_selected_category}`)// Access the document corresponding to the provided category.
+        .get() // Retrieve the data from the document.
+        .then(doc => {
+            category_data = doc.data(); // Store the retrieved data in a variable.
 
-    // Check if the selected category is 'category_1'.
-    if (user_selected_category == 'category_1') {
-        var added_script = document.createElement('script')
-        added_script.src = "scripts/search_garbage_subcategory.js"
-        document.body.appendChild(added_script)
-    }
+            // Extract the keys (subcategories) from the data and store them in an array.
+            const fields = Object.keys(category_data)
 
-    // Similar if conditions for other categories ('category_2' to 'category_5').
-    // Each condition creates a script element, sets its source to the respective script file,
-    // and appends it to the document body.
+            // Sort the array of subcategories alphabetically.
+            var ordered_fields = fields.sort()
+            console.log(ordered_fields) // Log the sorted subcategories for debugging.
+            console.log(ordered_fields[0])
 
-    if (user_selected_category == 'category_2') {
-        var added_script = document.createElement('script')
-        added_script.src = "scripts/search_glass_subcategory.js"
-        document.body.appendChild(added_script)
-    }
-    if (user_selected_category == 'category_3') {
-        var added_script = document.createElement('script')
-        added_script.src = "scripts/search_hazardous_subcategory.js"
-        document.body.appendChild(added_script)
-    }
-    if (user_selected_category == 'category_4') {
-        var added_script = document.createElement('script')
-        added_script.src = "scripts/search_metal_subcategory.js"
-        document.body.appendChild(added_script)
-    }
-    if (user_selected_category == 'category_5') {
-        var added_script = document.createElement('script')
-        added_script.src = "scripts/search_plastic_subcategory.js"
-        document.body.appendChild(added_script)
-    }
+            // Iterate over the sorted subcategories.
+            for (i = 0; i <= ordered_fields.length; i++)
+                // Update the HTML content of each button with the subcategory name.
+                // Replace underscores in subcategory names with spaces for display.
+                $(`.button${i}`).html(ordered_fields[i].replace("_", " "))
+        })
 }
 
-// Execute the function to load the appropriate script for the user-selected category.
-dynamically_load_correct_script()
+// dynamically load subcategories based on user-selected category
+display_material_subcategories("materials", user_selected_category)
--- scripts/material_categories.js
+++ scripts/material_categories.js
@@ -1,101 +1,137 @@
-// Function to redirect the user to the subcategories page.
+// // Function to redirect the user to the subcategories page.
+// function redirect_user_to_subcategories() {
+//     window.location.href = 'material_subcategories.html'
+// }
+
+// // Functions to store the selected category in local storage.
+// // Each function corresponds to a different category.
+
+// // Store 'category_1' in local storage.
+// function store_category_1_in_local_storage() {
+//     var category = 'category_1';
+//     localStorage.setItem('user_selected_category', category)
+// }
+
+// // Store 'category_2' in local storage.
+// function store_category_2_in_local_storage() {
+//     var category = 'category_2';
+//     localStorage.setItem('user_selected_category', category)
+// }
+
+// // Store 'category_3' in local storage.
+// function store_category_3_in_local_storage() {
+//     var category = 'category_3';
+//     localStorage.setItem('user_selected_category', category)
+// }
+
+// // Store 'category_4' in local storage.
+// function store_category_4_in_local_storage() {
+//     var category = 'category_4';
+//     localStorage.setItem('user_selected_category', category)
+// }
+
+// // Store 'category_5' in local storage.
+// function store_category_5_in_local_storage() {
+//     var category = 'category_5';
+//     localStorage.setItem('user_selected_category', category)
+// }
+
+// // Store 'category_6' in local storage.
+// function store_category_6_in_local_storage() {
+//     var category = 'category_6';
+//     localStorage.setItem('user_selected_category', category)
+// }
+
+// // Event listeners for each category button. When a category button is clicked,
+// // the corresponding category is stored in local storage, and the user is redirected.
+
+// // Listener for clicks on 'category_1' button.
+// document.addEventListener("click", function (e) {
+//     const category_button = e.target.closest(".category_1");
+//     if (category_button) {
+//         console.log("working!");
+//         store_category_1_in_local_storage();
+//         redirect_user_to_subcategories()
+//     }
+// })
+
+// // Similar event listeners are set up for 'category_2' to 'category_6' buttons.
+// // Each listener checks if the clicked element is a button for the respective category,
+// // then calls the appropriate function to store the category and redirects the user.
+
+// document.addEventListener("click", function (e) {
+//     const category_button = e.target.closest(".category_2");
+//     if (category_button) {
+//         console.log("working!");
+//         store_category_2_in_local_storage();
+//         redirect_user_to_subcategories()
+//     }
+// })
+// document.addEventListener("click", function (e) {
+//     const category_button = e.target.closest(".category_3");
+//     if (category_button) {
+//         console.log("working!");
+//         store_category_3_in_local_storage();
+//         redirect_user_to_subcategories()
+//     }
+// })
+// document.addEventListener("click", function (e) {
+//     const category_button = e.target.closest(".category_4");
+//     if (category_button) {
+//         console.log("working!");
+//         store_category_4_in_local_storage();
+//         redirect_user_to_subcategories()
+//     }
+// })
+// document.addEventListener("click", function (e) {
+//     const category_button = e.target.closest(".category_5");
+//     if (category_button) {
+//         console.log("working!");
+//         store_category_5_in_local_storage();
+//         redirect_user_to_subcategories()
+//     }
+// })
+// document.addEventListener("click", function (e) {
+//     const category_button = e.target.closest(".category_6");
+//     if (category_button) {
+//         console.log("working!");
+//         store_category_6_in_local_storage();
+//         redirect_user_to_subcategories()
+//     }
+// })
+
+function display_categories() {
+    var firestore_materials_collection = firebase.firestore().collection("materials");
+    firestore_materials_collection
+        .get()
+        .then((materials) => {
+            var materials_array = materials.docs.map(doc => doc.id);
+            console.log("materials_array: ", materials_array)
+            document.getElementsByClassName('category_1')[0].innerHTML = materials_array[0];
+            document.getElementsByClassName('category_2')[0].innerHTML = materials_array[1];
+            document.getElementsByClassName('category_3')[0].innerHTML = materials_array[2];
+            document.getElementsByClassName('category_4')[0].innerHTML = materials_array[3];
+            document.getElementsByClassName('category_5')[0].innerHTML = materials_array[4];
+        })
+}
+
+display_categories();
+
 function redirect_user_to_subcategories() {
     window.location.href = 'material_subcategories.html'
 }
 
-// Functions to store the selected category in local storage.
-// Each function corresponds to a different category.
-
-// Store 'category_1' in local storage.
-function store_category_1_in_local_storage() {
-    var category = 'category_1';
-    localStorage.setItem('user_selected_category', category)
-}
-
-// Store 'category_2' in local storage.
-function store_category_2_in_local_storage() {
-    var category = 'category_2';
-    localStorage.setItem('user_selected_category', category)
-}
-
-// Store 'category_3' in local storage.
-function store_category_3_in_local_storage() {
-    var category = 'category_3';
-    localStorage.setItem('user_selected_category', category)
-}
-
-// Store 'category_4' in local storage.
-function store_category_4_in_local_storage() {
-    var category = 'category_4';
-    localStorage.setItem('user_selected_category', category)
-}
-
-// Store 'category_5' in local storage.
-function store_category_5_in_local_storage() {
-    var category = 'category_5';
-    localStorage.setItem('user_selected_category', category)
-}
-
-// Store 'category_6' in local storage.
-function store_category_6_in_local_storage() {
-    var category = 'category_6';
-    localStorage.setItem('user_selected_category', category)
-}
-
-// Event listeners for each category button. When a category button is clicked,
-// the corresponding category is stored in local storage, and the user is redirected.
-
-// Listener for clicks on 'category_1' button.
 document.addEventListener("click", function (e) {
-    const category_button = e.target.closest(".category_1");
+    const category_button = e.target.closest(".main_button");
     if (category_button) {
         console.log("working!");
-        store_category_1_in_local_storage();
         redirect_user_to_subcategories()
+        store_category(category_button)
     }
 })
 
-// Similar event listeners are set up for 'category_2' to 'category_6' buttons.
-// Each listener checks if the clicked element is a button for the respective category,
-// then calls the appropriate function to store the category and redirects the user.
+function store_category(category_button) {
+    category_name = category_button.innerHTML
+    localStorage.setItem("user_selected_category", category_name)
+}
 
-document.addEventListener("click", function (e) {
-    const category_button = e.target.closest(".category_2");
-    if (category_button) {
-        console.log("working!");
-        store_category_2_in_local_storage();
-        redirect_user_to_subcategories()
-    }
-})
-document.addEventListener("click", function (e) {
-    const category_button = e.target.closest(".category_3");
-    if (category_button) {
-        console.log("working!");
-        store_category_3_in_local_storage();
-        redirect_user_to_subcategories()
-    }
-})
-document.addEventListener("click", function (e) {
-    const category_button = e.target.closest(".category_4");
-    if (category_button) {
-        console.log("working!");
-        store_category_4_in_local_storage();
-        redirect_user_to_subcategories()
-    }
-})
-document.addEventListener("click", function (e) {
-    const category_button = e.target.closest(".category_5");
-    if (category_button) {
-        console.log("working!");
-        store_category_5_in_local_storage();
-        redirect_user_to_subcategories()
-    }
-})
-document.addEventListener("click", function (e) {
-    const category_button = e.target.closest(".category_6");
-    if (category_button) {
-        console.log("working!");
-        store_category_6_in_local_storage();
-        redirect_user_to_subcategories()
-    }
-})
