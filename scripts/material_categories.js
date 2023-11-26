06e1462204d9de267a417e243a34291629f9b1e2
Sebastian Taylor <staylor173@my.bcit.ca>
2023-11-26 14:11:48 -0800

    load category buttons to remove empty buttons

--- scripts/material_categories.js
+++ scripts/material_categories.js
@@ -1,32 +1,39 @@
+// categories.js
+
 function display_categories() {
     let firestore_materials_collection = firebase.firestore().collection("materials");
     firestore_materials_collection.get().then((materials) => {
         let materials_array = materials.docs.map((doc) => doc.id);
         console.log("materials_array: ", materials_array);
-        document.getElementsByClassName("category_1")[0].innerHTML = materials_array[0];
-        document.getElementsByClassName("category_2")[0].innerHTML = materials_array[1];
-        document.getElementsByClassName("category_3")[0].innerHTML = materials_array[2];
-        document.getElementsByClassName("category_4")[0].innerHTML = materials_array[3];
-        document.getElementsByClassName("category_5")[0].innerHTML = materials_array[4];
+
+        const categoriesContainer = document.getElementById("categoriesContainer");
+        categoriesContainer.innerHTML = "";
+
+        materials_array.forEach((material, index) => {
+            const button = document.createElement("button");
+            const buttonText = document.createTextNode(material);
+            button.appendChild(buttonText); // Append the text node to the button
+
+            // Add the appropriate classes including a specific one for categories page
+            button.className = `main_button category_button category_${index + 1}`;
+
+            // Set the onclick event to store the category and redirect
+            button.onclick = () => {
+                store_category_and_redirect(button);
+            };
+
+            // Append the button to the container
+            categoriesContainer.appendChild(button);
+        });
     });
 }
 
-display_categories();
-
-function redirect_user_to_subcategories() {
+function store_category_and_redirect(category_button) {
+    // Get the category name, which is the button's text content
+    const category_name = category_button.textContent;
+    localStorage.setItem("user_selected_category", category_name);
     window.location.href = "material_subcategories.html";
 }
 
-document.addEventListener("click", function (e) {
-    const category_button = e.target.closest(".main_button");
-    if (category_button) {
-        console.log("working!");
-        redirect_user_to_subcategories();
-        store_category(category_button);
-    }
-});
-
-function store_category(category_button) {
-    category_name = category_button.innerHTML;
-    localStorage.setItem("user_selected_category", category_name);
-}
+// Call the function to display categories
+display_categories();
--- scripts/material_subcategories.js
+++ scripts/material_subcategories.js
@@ -1,17 +1,44 @@
-function redirect_user_to_display_material_information() {
+// subcategories.js
+
+function display_material_subcategories(collection) {
+    const user_selected_category = localStorage.getItem("user_selected_category");
+    console.log("Selected Category:", user_selected_category);
+
+    db.collection(collection)
+        .doc(user_selected_category)
+        .get()
+        .then((doc) => {
+            const category_data = doc.data();
+            console.log("Category data:", category_data);
+
+            if (!category_data) {
+                console.error('No data found for category:', user_selected_category);
+                return;
+            }
+
+            const fields = Object.keys(category_data).sort();
+
+            const subcategoriesContainer = document.getElementById("subcategoriesContainer");
+            subcategoriesContainer.innerHTML = "";
+
+            fields.forEach((field, index) => {
+                const button = document.createElement("button");
+                button.innerHTML = field.replace(/_/g, " ");
+                // Add main_button and subcategory_button classes for styling
+                button.className = `main_button subcategory_button button${index}`;
+                button.onclick = () => {
+                    store_subcategory_and_redirect(button);
+                };
+                subcategoriesContainer.appendChild(button);
+            });
+        });
+}
+
+function store_subcategory_and_redirect(subcategory_button) {
+    const subcategory_name = subcategory_button.innerText;
+    localStorage.setItem("user_selected_subcategory", subcategory_name);
     window.location.href = "display_material_information.html";
 }
 
-function store_subcategory(subcategory_button) {
-    subcategory_name = subcategory_button.innerHTML;
-    localStorage.setItem("user_selected_subcategory", subcategory_name);
-}
-
-document.addEventListener("click", function (e) {
-    const subcategory_button = e.target.closest("#individual_buttons");
-    if (subcategory_button) {
-        console.log("working!");
-        store_subcategory(subcategory_button);
-        redirect_user_to_display_material_information();
-    }
-});
+// Assuming you have a user-selected category in local storage
+display_material_subcategories("materials");
